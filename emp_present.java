7d564419d994e527cd347d75a8523c6ece941c65
SopanBhagat27 <41367620+SopanBhagat27@users.noreply.github.com>
2020-07-14 23:57:59 +0530

    Add files via upload

---
+++ emp_present.java
@@ -0,0 +1,16 @@
+public class emp_present
+{
+    public static void main(String[] args)
+    {
+         int is_present=1;
+                double emp = Math.floor(Math.random() * 10) % 2;
+                if (is_present == emp )
+                        {
+                                System.out.println("employee is present");
+                        }
+                else
+                        {
+                                System.out.println("employee is absent");
+                        }
+    }
+}
